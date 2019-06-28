fa47d4fd82641109e15f2f34961c5b94721828cd
archie <arseniy.tarapon@gmail.com>
2019-06-29 03:28:13 +0600

    Создал приложение accounts. Подключил логинку.

--- source/main/settings.py
+++ source/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
