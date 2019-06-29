2aa46ed17e32fa2f5a6e17aaed18f11c1210ce1c
archie <arseniy.tarapon@gmail.com>
2019-06-29 06:29:23 +0600

    Переработка форм. Для добавления классов бутстрапа полям из форм, подключена библиотека django-widget-tweaks. Фильтр {{ form.field|add_class:"class class class" }} добавляет классы к виджету поля form.field (вместо "field" подставить нужное поле, вместо "class class class" - ваши классы). Переработка меню авторизации.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -3,13 +3,6 @@ from .models import Article
 
 
 class ArticleForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
-            if visible.name == 'image':
-                visible.field.widget.attrs['class'] = 'form-control-file'
-
     def clean(self):
         if not self.cleaned_data['text'] and not self.cleaned_data['image']:
             raise forms.ValidationError('Хотя бы одно из полей "Текст" и "Картинка" должно быть заполнено.')
